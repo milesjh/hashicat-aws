9ed2097743682f70d0560a3b4831ec086d64ab61
Miles Harrison <harrison.milesj@gmail.com>
2021-09-16 18:18:26 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "Miles-CNCY"
     workspaces {
       name = "hashicat-aws"
